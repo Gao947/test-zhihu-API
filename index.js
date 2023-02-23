8f4b3d761af0afea918f95fcabfddc705f3aae66
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-23 20:05:59 +1100

    more middlewares

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+const usersRouter = new Router({ prefix: '/users' });
+
+const auth = async (ctx, next) => {
+    if(ctx.url !== '/users'){
+    ctx.throw(401);   
+    }
+    await next();
+};
 
 router.get('/', (ctx) => {
     ctx.body = 'This is MainPage';
 });
 
-router.get('/users', (ctx)=>{
+usersRouter.get('/', auth, (ctx)=>{
     ctx.body = 'This is User List';
 });
 
-router.post('/users', (ctx)=>{
+usersRouter.post('/', auth, (ctx)=>{
     ctx.body = 'Create Users';
 });
 
-router.get('/users/:id', (ctx)=>{
+usersRouter.get('/:id', auth, (ctx)=>{
     ctx.body = `This is user ${ctx.params.id}`;
 })
 
 app.use(router.routes());
+app.use(usersRouter.routes());
 
 app.listen(2000);
