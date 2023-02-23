6b6ed47db56b6816a2eb89b7d8dc7d8f17f21510
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-23 15:05:06 +1100

    onion module

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use((ctx) => {
-    ctx.body = 'Hello Zhihu!';
+app.use(async (ctx, next) => {
+    console.log(1)
+    await next();
+    console.log(2);
+    ctx.body = 'Hello Zhihu API';
+});
+app.use(async (ctx, next) => {
+    console.log(3);
+    await next();
+    console.log(4)
+});
+app.use(async (ctx) => {
+    console.log(5);
 });
 
 app.listen(2000);
