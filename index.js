f3b06d1858f2f5405c53621f2473fb43c0e7db23
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-24 11:30:52 +1100

    HTTP response CRUD

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
 const Koa = require('koa');
+const bodyparser = require('koa-bodyparser');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 const usersRouter = new Router({ prefix: '/users' });
 
-const auth = async (ctx, next) => {
-    if(ctx.url !== '/users'){
-    ctx.throw(401);   
-    }
-    await next();
-};
+const db = [{ name: "Mike" }]
 
 router.get('/', (ctx) => {
-    ctx.body = 'This is MainPage';
+    ctx.body = '<h1>This is HomePage</h1>'; //这是主页
 });
 
-usersRouter.get('/', auth, (ctx)=>{
-    ctx.body = 'This is User List';
+usersRouter.get('/', (ctx)=>{
+    ctx.body =db; //这是用户列表页
 });
 
-usersRouter.post('/', auth, (ctx)=>{
-    ctx.body = 'Create Users';
+usersRouter.post('/', (ctx)=>{
+    db.push(ctx.request.body);
+    ctx.body = ctx.request.body; //创建用户
 });
 
-usersRouter.get('/:id', auth, (ctx)=>{
-    ctx.body = `This is user ${ctx.params.id}`;
-})
+usersRouter.get('/:id', (ctx)=>{
 
+    ctx.body = db[ctx.params.id * 1]; //这是用户
+});;
+
+usersRouter.put('/:id', (ctx)=>{
+    db[ctx.params.id * 1] = ctx.request.body;
+    ctx.body = ctx.request.body; //修改用户
+});
+
+usersRouter.delete('/:id', (ctx)=>{
+    db.splice(ctx.params.id * 1, 1);
+    ctx.status = 204; //删除用户
+});
+
+app.use(bodyparser());
 app.use(router.routes());
 app.use(usersRouter.routes());
 app.use(usersRouter.allowedMethods());
