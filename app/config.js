79b884359ba525b52cc173dfd0ed8571f4445a96
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-26 20:37:12 +1100

    7 Mongoos DB CRUD

---
+++ app/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    connnectionStr: 'mongodb+srv://gaodg947:Gdy961019!@zhihu.53rmiga.mongodb.net/?retryWrites=true&w=majority'
+};
--- app/index.js
+++ app/index.js
@@ -1,9 +1,20 @@
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser');
+const error = require('koa-json-error');
+const parameter = require('koa-parameter');
+const mongoose = require('mongoose');
 const app = new Koa();
-const routing = require('./routes')
+const routing = require('./routes');
+const { connnectionStr } = require('./config'); 
 
+mongoose.connect(connnectionStr, ()=> console.log('MongoDB 连接成功了！'));
+mongoose.connection.on('error', console.error);
+
+app.use(error({
+    postFormat: (e, {stack, ...rest})=>process.env.NODE_ENV === 'production' ? rest: {stack, ...rest}
+}));
 app.use(bodyparser());
+app.use(parameter(app));
 routing(app);
 
 app.listen(2000, () => console.log('程序启动在 2000 端口了'));
