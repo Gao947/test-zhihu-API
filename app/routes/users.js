015d2e28edd9610455c75b844a0d29f4b2f36688
DESKTOP-BD14OH8\Diya GAO <gaodg947@gmail.com>
2023-02-27 17:57:18 +1100

    authorization and checkOwner

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,11 +1,24 @@
+const jsonwebtoken = require('jsonwebtoken');
 const Router = require('koa-router');
 const router = new Router({prefix: '/users'});
 const { 
     find, findById, create, update, 
-    delete: del, login 
-} = require('../contrallers/users')
+    delete: del, login, checkOwner
+} = require('../contrallers/users');
 
+const { secret } = require('../config');
 
+const auth = async (ctx, next) => {
+    const { authorization = '' } = ctx.request.header;
+    const token = authorization.replace('Bearer ', '');
+    try {
+      const user = jsonwebtoken.verify(token, secret);
+      ctx.state.user = user;
+    } catch (err) {
+      ctx.throw(401, err.message);
+    }
+    await next();
+}
 
 router.get('/', find);
 
@@ -13,9 +26,9 @@ router.post('/', create);
 
 router.get('/:id', findById);
 
-router.patch('/:id', update);
+router.patch('/:id', auth, checkOwner, update);
 
-router.delete('/:id', del);
+router.delete('/:id', auth, checkOwner, del);
 
 router.post('/login', login);
 
